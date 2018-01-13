5e2f761e064f066904970312c28fef565a4c3f80
Mathias <mathiasfriis1995@hotmail.com>
2018-01-13 15:08:00 +0100

    Til og med 7.2

---
+++ Agent Assigments/Agent Assignments - 7/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Agent_Assignments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Agent_Assigments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer Timer = new DispatcherTimer();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+        }
+
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d;
+            d = DateTime.Now;
+            LabelTime.Content = d.Hour + " : " + d.Minute + " : " + d.Second;
+        }
+    }
+}
