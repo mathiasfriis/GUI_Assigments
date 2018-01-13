ff455d3009a65448ab6fe15d35bc452d0360d316
Mathias <mathiasfriis1995@hotmail.com>
2018-01-13 16:14:47 +0100

    7.2

---
+++ Agent Assigments/Agent Assignments - 8/AgentIdToColorValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Diagnostics;
+
+namespace Agent_Assignments
+{
+    class AgentIdToColorValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debug.Assert(targetType == typeof(Brush));
+            string id = value as string;
+            if (id == null)
+                id = "";
+            // 007 requires special treatment...
+            return (id == "007" ? Brushes.Blue : Brushes.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
